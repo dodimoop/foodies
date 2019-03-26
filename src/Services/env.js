ac8285a7b97659ff7dd183fef9ed96cd57949c60
dodimoop <dody.setiyawan11@gmail.com>
2019-03-26 19:39:56 +0700

    Make a env file for  setup BASE_URL and API_KEY

---
+++ src/Services/env.js
@@ -0,0 +1,5 @@
+//  baseURL for the API Request
+export const BASE_URL  = 'https://developers.zomato.com/api'
+
+// Key for request to API
+export const API_KEY   = 'e57f4758333fc093b1fc4c40801bd7b8'
