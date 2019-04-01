2a7ef9ba523b08c844033d2e09b2ab7e5882a58e
dodimoop <dody.setiyawan11@gmail.com>
2019-04-01 17:29:49 +0700

    change API KEY

--- src/Services/env.js
+++ src/Services/env.js
@@ -2,6 +2,6 @@
 export const BASE_URL  = 'https://developers.zomato.com/api'
 
 // Key for request to API
-export const API_KEY   = '1a9d3945ff152c9a8b1872a43e723db0'
+export const API_KEY   = 'e57f4758333fc093b1fc4c40801bd7b8'
 
-// e57f4758333fc093b1fc4c40801bd7b8
+// 1a9d3945ff152c9a8b1872a43e723db0
