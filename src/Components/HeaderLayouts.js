fc911e61ae1d3d9a7769315679a0be180f445025
dodimoop <dody.setiyawan11@gmail.com>
2019-04-02 17:30:41 +0700

    Add style for Header

---
+++ src/Components/HeaderLayouts.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Header, Segment, Image } from 'semantic-ui-react'
+import classes from './HeaderLayouts.module.scss';
+
+const HeaderLayouts = () => (
+  <Segment className={classes.HeaderLayouts} clearing>
+    <Header floated='left'>
+      <h1 className={classes.colorText}>
+        F O O D I E S .
+      </h1>
+    </Header>
+  </Segment>
+)
+
+export default HeaderLayouts
