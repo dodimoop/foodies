bc56328cc24ba0e197b193e5d636c9d46d437279
dodimoop <dody.setiyawan11@gmail.com>
2019-04-04 18:06:59 +0700

    Implementation Route and add Result Page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
 import './App.module.scss'
-// import HeaderLayouts from './Components/HeaderLayouts'
-import HomePages from './Pages/HomePages'
-// import { Switch, Route, Redirect } from 'react-router-dom'
+import HomePages from './Pages/HomePages/HomePages'
+import ResultPages from './Pages/ResultPages/ResultPages'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <HeaderLayouts /> */}
-        <HomePages />
+      <Switch>
+        <Route path="/" exact component={HomePages} />
+        <Route path="/result" component={ResultPages}/>
+        {/* <Route path="/detail-result/" component={resultPage}/> */}
+        <Route path="*" render={() => <Redirect to="/"/>}/>
+      </Switch>
       </div>
     );
   }
