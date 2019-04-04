aad23ca37093983326d1373e895e3795f968c240
dodimoop <dody.setiyawan11@gmail.com>
2019-04-04 18:07:22 +0700

    Remove Image

--- src/Components/HeaderLayouts.js
+++ src/Components/HeaderLayouts.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Header, Segment, Image } from 'semantic-ui-react'
+import { Header, Segment } from 'semantic-ui-react'
 import classes from './HeaderLayouts.module.scss';
 
 const HeaderLayouts = () => (
