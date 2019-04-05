91f77f83abb2c3688956cb8953c08162490d3ec4
dodimoop <dody.setiyawan11@gmail.com>
2019-04-05 17:25:40 +0700

    Add DetailPages file for restaurant details

---
+++ src/Pages/DetailPages/DetailPages.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import classes from 'DetailPages.module.scss';
+
+class DetailPages extends Component {
+  render() {
+    return (
+      <div className={classes.detailPages}>
+
+      </div>
+    )
+  }
+}
+
+export default DetailPages
