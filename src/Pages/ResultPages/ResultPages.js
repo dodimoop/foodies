a06ffaa26433dc74a0ea223bce33170c55158f4b
dodimoop <dody.setiyawan11@gmail.com>
2019-04-08 16:25:31 +0700

    Add page and + restaurants when click loadmore

--- src/Pages/ResultPages/ResultPages.js
+++ src/Pages/ResultPages/ResultPages.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import queryString from 'query-string'
 import HeaderLayout from '../../Components/HeaderLayouts'
 import API from '../../Services/services'
-import { Grid, Image, Card, Dimmer, Loader, Label } from 'semantic-ui-react'
+import { Grid, Image, Card, Dimmer, Loader, Label, Button, Icon } from 'semantic-ui-react'
 import classes from './ResultPages.module.scss';
 
 class ResultPages extends Component {
@@ -10,6 +10,9 @@ class ResultPages extends Component {
   state = {
     restaurants: [],
     isLoading: false,
+    page: 0,
+    isLoadingLoadmore: false,
+    count: 6
   }
 
   async componentDidMount() {
@@ -19,22 +22,21 @@ class ResultPages extends Component {
   isFetchData = async () => {
     try {
       let dataQueryString = queryString.parse(this.props.location.search)
-  
       this.setState({restaurants: [], isLoading: true})
-        
         let response = await API.get('v2.1/search', {
           params: {
             entity_id: dataQueryString.cityId,
             entity_type: "city",
             q: dataQueryString.q,
-            count: 6
+            page: this.state.page,
+            count: this.state.count
           }
         })
 
         if (response) {
           await this.setState({restaurants: response.data.restaurants, isLoading: false})
         }
-        console.log(this.state.restaurants);
+        
     } catch (error) {
       console.log(error);
     }
@@ -45,6 +47,38 @@ class ResultPages extends Component {
     window.localStorage.clear()
     window.localStorage.setItem('currentRestaurant', JSON.stringify(data))
     this.props.history.push(`restaurant/${idRestaurant}`)
+  }
+
+  isLoadmore = async () => {
+    try {
+      await this.setState({restaurants: [], isLoading: true, isLoadingLoadmore: true, count: this.state.count + 6})
+      let dataQueryString = queryString.parse(this.props.location.search)
+      this.props.history.push(`/result?cityId=${dataQueryString.cityId}&q=${dataQueryString.q}&page=${this.state.page + 1}`)
+      let response = await API.get('v2.1/search', {
+        params: {
+          entity_id: dataQueryString.cityId,
+          entity_type: "city",
+          q: dataQueryString.q,
+          page: this.state.page,
+          count: this.state.count
+        }
+      })
+      if (response) {
+        await this.setState({restaurants: response.data.restaurants, isLoading: false, isLoadingLoadmore: false})
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  backHome = async () => {
+    try {
+      await this.setState({restaurants: []})
+      this.props.history.push('/')
+      console.log(this.state.restaurants);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   render() {
@@ -83,6 +117,17 @@ class ResultPages extends Component {
             {dataRestaurants}
           </Grid.Row>
         </Grid>
+        <div className={classes.buttonLoadmore}>
+          <Button onClick={this.isLoadmore} loading={this.state.isLoadingLoadmore}>
+            Load More
+          </Button>
+        </div>
+        <div className={classes.buttonBack}>
+          <Button icon labelPosition='left' onClick={this.backHome} color="red">
+            Back to Home Page
+            <Icon name='left arrow' />
+          </Button>
+        </div>
       </div>
     )
   }
