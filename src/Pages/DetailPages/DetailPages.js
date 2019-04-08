548787c9b6bfa5f31340a6ba6fc438df9d6d260b
dodimoop <dody.setiyawan11@gmail.com>
2019-04-08 18:53:48 +0700

    WIP - details page

--- src/Pages/DetailPages/DetailPages.js
+++ src/Pages/DetailPages/DetailPages.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
-import classes from 'DetailPages.module.scss';
+import classes from './DetailsPages.module.scss'
+import HeaderLayouts from '../../Components/HeaderLayouts'
+import { Grid, Image} from 'semantic-ui-react'
 
 class DetailPages extends Component {
   render() {
     return (
       <div className={classes.detailPages}>
-
+        <HeaderLayouts match={this.props.match}/>
+        <Grid columns={2}>
+          <Grid.Row>
+            <Grid.Column>
+              <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+            </Grid.Column>
+            <Grid.Column>
+              <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </div>
     )
   }
